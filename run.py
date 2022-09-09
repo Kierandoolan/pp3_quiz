ae4506adc4e81164c71f22d3a391db03cf756c8a
Kierandoolan <kierandoolan91@gmail.com>
2022-09-09 12:49:07 +0000

    fix errors for pep8online

--- run.py
+++ run.py
@@ -1,26 +1,25 @@
-"""
-import section (get color)
-"""
-
+import colorama
+from colorama import Fore
+colorama.init(autoreset=True)
 
 quiz_info = [
     {"question": "Q1. What is the Capital of Ireland:",
      "answers": {"A": "Galway",
                  "B": "Dublin",
                  "C": "Cork",
-		         "D": "Galway"},
+                 "D": "Galway"},
      "correct": "B"},
     {"question": "Q2. Who Created the Xbox:",
      "answers": {"A": "Sony",
                  "B": "Apple",
                  "C": "Nintendo",
-		         "D": "Microsoft"},
+                 "D": "Microsoft"},
      "correct": "D"},
     {"question": "Q3. Where is the Car manufacturers Audi From:",
      "answers": {"A": "Germany",
                  "B": "China",
                  "C": "USA",
-		         "D": "London"},
+                 "D": "London"},
      "correct": "A"},
 
     {"question": "Q4. Which of these is a Song from Red Hot Chilli Peppers:",
@@ -33,36 +32,36 @@ quiz_info = [
      "answers": {"A": "Poland",
                  "B": "Denmark",
                  "C": "Sweeden",
-		         "D": "All of the Above"},
+                 "D": "All of the Above"},
      "correct": "D"},
     {"question": "Q6. What color dresses do Chinese women traditionally wear on their wedding day? ",
      "answers": {"A": "Blue",
                  "B": "Gold",
                  "C": "White",
-		         "D": "Red"},
+                 "D": "Red"},
      "correct": "D"},
     {"question": "Q7. How many molecules of oxygen does ozone have?",
      "answers": {"A": "One",
                  "B": "Two",
                  "C": "Three",
-		         "D": "Four"},
+                 "D": "Four"},
      "correct": "C"},
     {"question": "Q8. Where did the croissant originate?",
      "answers": {"A": "France",
                  "B": "Austria",
                  "C": "Turkey",
-		         "D": "Tokyo"},
+                 "D": "Tokyo"},
      "correct": "B"},
     {"question": "Q9. Pupusas are from what country",
      "answers": {"A": "Mexico",
                  "B": "El Salvador",
                  "C": "Brazil",
-		         "D": "Poland"},
+                 "D": "Poland"},
      "correct": "B"},
     {"question": "Final Question.  Which of these US presidents appeared on the television series 'Laugh-In'  ",
      "answers": {"A": "Lyndon Johnson",
                  "B": "Richard Nixon",
-		         "C": "Jimmy Carter",
+                 "C": "Jimmy Carter",
                  "D": "Gerald Ford"},
      "correct": "B"},
 ]
@@ -76,26 +75,24 @@ def introduction():
     """
     name = input("Please enter your name: \n")
 
-    print (f"Welcome to the Trivia Quiz {name} where we test your knowledge, \n")
-    print ("There are 10 questions that gets harder with each question answered, \n")
-    print ("There are four choices for each question \n")
+    print (f"Welcome to the Trivia Quiz {name} where we test your knowledge ")
+    print ("There are 10 questions that gets harder with each question answered,")
+    print ("There are four choices for each question ")
     print ("Please Pick A, B, C or D when answering \n")
 
-    start= True
+    start = True
     while start:
         answer = input(f"Are you ready to begin the quiz {name} Y/N :")
-
         if answer.upper() == "Y":
-            
-            print('Lets Play the Quiz!')
+            print('Lets Play the Quiz! \n')
             break
         elif answer.upper() == "N":
             print('not ready')
-        else :
+        else:
             print('invalid')
 
-def start_quiz(quiz_info):
 
+def start_quiz(quiz_info):
     """
     Function to loop through all questions and possible answers
     when the user gets the answer correct 100euro is added to their score.
@@ -113,26 +110,34 @@ def start_quiz(quiz_info):
             answer = answer.upper()
 
             if answer not in info['answers']:
-                print("Invalid Choice. Please try again with the correct letters\n")
+                print("Invalid Choice. Please try again with the\
+                correct letters\n")
         if answer == info['correct']:
             cash += 100
-            print(f"Well done thats the correct answer ! You gain {cash} euro \n")
+            print(Fore.GREEN + f"Well done thats the correct answer!\
+            You gain {cash} euro \n")
         else:
-            print("Wrong answer.")
+            print(Fore.RED + " Oh No! Wrong answer.")
 
     end_of_quiz(cash)
+
 
 def end_of_quiz(cash):
     """
     Function to tell the user of its overall score
     """
-    print(f"Well done overall you have made {cash}. Congraulations!")
+    if cash == 1000:
+        print(Fore.YELLOW + f"Well done you got the top prize of {cash}euro.Congraulations!")
+    elif cash == 0:
+        print(Fore.RED + f"Oh no you didn't get any questions right.You have zero euro!")
+    else:
+        print(Fore.GREEN + f"Well done overall you have made {cash} euro.Congraulations!")
     restart_game()
 
 
 def restart_game():
     """
-    Function to ask the user if they would like to restart the game 
+    Function to ask the user if they would like to restart the game
     or to finish the game
     """
     answer = input("Would you like to try and beat your score? Y/N: ")
@@ -143,8 +148,6 @@ def restart_game():
     else:
         print("invalid Choice")
         restart_game()
-        
 
 introduction()
 start_quiz(quiz_info)
-
